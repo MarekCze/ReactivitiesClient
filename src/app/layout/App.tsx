67a06edbce7fbc5ff7a3cd9185cb35082fdfea06
Marek Czerniejewski <marekcze@outlook.com>
2023-03-09 19:20:59 +0000

    completed section 5

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,13 +1,17 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './Navbar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import {v4 as uuid} from 'uuid';
 //import List from 'semantic-ui-react/dist/commonjs/elements/List';
 
 function App() {
+  // useState of type Activity[] (Activity array), with default of empty array
   const [activities, setActivities] = useState<Activity[]>([]);
-  
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
+
   useEffect(() => {
     axios.get<Activity[]>('http://localhost:5000/api/activities')
     .then(response => {
@@ -16,16 +20,53 @@ function App() {
     })
   }, [])
 
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(activities.find(x => x.id === id));
+  }
+
+  function handleCancelSelectActivity() {
+    setSelectedActivity(undefined);
+  }
+
+  function handleFormOpen(id?: string) {
+    id ? handleSelectActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose() {
+    setEditMode(false);
+  }
+
+  function handleCreateOrEditActivity(activity: Activity) {
+    // if there is an id, edit activity with that id
+    // else create new activity
+    activity.id 
+      ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
+      : setActivities([...activities, {...activity, id: uuid()}]);
+
+    setEditMode(false);
+    setSelectedActivity(activity);
+  }
+
+  function handleDeleteActivity(id: string) {
+    // only leave activities where id !== passed id
+    setActivities([...activities.filter(x => x.id !== id)])
+  }
+
   return (
     <Fragment>
-      <NavBar />
-      <List>
-        {activities.map((activity) => (
-          <List.Item key={activity.id}>
-            {activity.title}
-          </List.Item>
-        ))}
-      </List>
+      <NavBar openForm={handleFormOpen} />
+      <ActivityDashboard
+        activities={activities}
+        selectedActivity={selectedActivity}
+        selectActivity={handleSelectActivity}
+        cancelSelectActivity={handleCancelSelectActivity}
+        editMode={editMode}
+        openForm={handleFormOpen}
+        closeForm={handleFormClose}
+        createOrEdit={handleCreateOrEditActivity}
+        deleteActivity={handleDeleteActivity}
+        />
     </Fragment>
   );
 }
